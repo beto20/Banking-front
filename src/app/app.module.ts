a9961258c54fbee084fbbb916507d87d935516eb
beto20 <alberto.vs@outlook.com>
2023-10-12 00:54:50 -0500

    add auth, pages and not found page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule
