338cd132f93314a65dce08a31db2fe92772c3eaa
beto20 <alberto.vs@outlook.com>
2023-10-28 12:45:25 -0500

    add routerlink and reactiveform for auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth.routing';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor(private readonly authService: AuthService, private readonly router: Router) {}
+
+  validateCredentials() {
+    console.log('Login username: ', this.loginForm.value.username)
+    console.log('Login password: ', this.loginForm.value.password)
+
+    const hasAccess = this.authService.getToken(this.loginForm.value.username , this.loginForm.value.password )
+
+    console.log(hasAccess)
+    if(hasAccess === true) {
+      this.router.navigateByUrl('/init')
+    }
+  }
 
 }
