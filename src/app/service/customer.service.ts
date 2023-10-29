09e8a23c2c5d80c5fed449ff88ac94a573b92877
beto20 <alberto.vs@outlook.com>
2023-10-29 01:47:37 -0500

    integrate with customer backend services

--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -7,11 +7,103 @@ export class CustomerService {
 
   constructor() { }
 
-
-  searchByDocument(docType: any, docNumber: any) {
+  registerCustomer(customerDto: any) {
 
     const token = sessionStorage.getItem('session')
     console.log("token:", token)
+
+    // DTO structure move this to the component
+    // const customerDto = { 
+    //   name: "",
+    //   middleName: "",
+    //   lastName: "",
+    //   motherLastName: "",
+    //   maritalStatus: "",
+    //   birthdate: "",
+    //   documentType: "",
+    //   documentNumber: "",
+    //   gender: "",
+    //   email: "",
+    //   phone: "",
+    // }
+
   }
 
+  searchByDocument(docType: any, docNumber: any): CustomerResponse {
+
+    const token = sessionStorage.getItem('session')
+    console.log("token:", token)
+
+    const result: CustomerResponse = { 
+      name: "",
+      middleName: "",
+      lastName: "",
+      motherLastName: "",
+      maritalStatus: "",
+      birthdate: "",
+      documentType: "",
+      documentNumber: "",
+      gender: "",
+      age: 1,
+      email: "",
+      phone: "",
+    }
+
+    return result;
+  }
+
+  getProducts(customer: any): ProductResponse[] {
+
+    const token = sessionStorage.getItem('session')
+    console.log("token:", token)
+
+    const result: ProductResponse[] = [
+      { 
+        title: "",
+        freeField1: "",
+        freeField2: "",
+        freeField3: "",
+        freeField4: "",
+        freeField5: "",
+        imageURL: "",
+      },
+      { 
+        title: "",
+        freeField1: "",
+        freeField2: "",
+        freeField3: "",
+        freeField4: "",
+        freeField5: "",
+        imageURL: "",
+      }
+    ]
+
+    return result;
+  }
+
+}
+
+export interface CustomerResponse { 
+  name: string;
+  middleName: string;
+  lastName: string;
+  motherLastName: string;
+  maritalStatus: string;
+  birthdate: string;
+  documentType: string;
+  documentNumber: string;
+  gender: string;
+  age: number;
+  email: string;
+  phone: string;
+}
+
+export interface ProductResponse { 
+  title: string;
+  freeField1: string;
+  freeField2: string;
+  freeField3: string;
+  freeField4: string;
+  freeField5: string;
+  imageURL: string;
 }
